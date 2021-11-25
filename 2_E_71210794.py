ba3f12e45149bb6a17e2002ad2006f2ea5eeab84
EvanderGab <evander.gabriel@ti.ukdw.ac.id>
2021-11-25 17:04:54 +0700

    Revisi Soal 2_E_71210794

--- 2_E_71210794.py
+++ 2_E_71210794.py
@@ -1,25 +1,28 @@
 kata= input("Masukan kata : ")
-x = len(kata)
 
-def kataTengah():
+def kataTengah(kata):
+    x = len(kata)
     if x % 2 == 1:
         if x >=5:
             y = int((x - 1)/2)
             jawab = kata[y-1]+kata[y]+kata[y+1]
-            print("Huruf tengah pada kata",kata,"adalah", str(jawab))
+            #print("Huruf tengah pada kata",kata,"adalah", str(jawab))
+            return jawab
         elif x<5:
             y = int((x - 1)/2)
             jawab = kata[y]
-            print("Huruf tengah pada kata",kata,"adalah", str(jawab))
-
+            #print("Huruf tengah pada kata",kata,"adalah", str(jawab))
+            return jawab
     elif x%2 ==0:
         if x < 8:
             y= int(x/2)
             jawab= kata[y-1]+kata[y]
-            print("Huruf tengah pada kata",kata,"adalah", str(jawab))
+            #print("Huruf tengah pada kata",kata,"adalah", str(jawab))
+            return jawab
         else:
             y=int(x/2)
             jawab=kata[y-2]+kata[y-1]+kata[y]+kata[y+1]
-            print("Huruf tengah pada kata",kata,"adalah", str(jawab))
+            #print("Huruf tengah pada kata",kata,"adalah", str(jawab))
+            return jawab
 
-kataTengah()
+print("Huruf tengah pada kata",kata,"adalah",kataTengah(kata))
